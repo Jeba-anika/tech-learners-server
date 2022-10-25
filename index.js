7224bff80af8a97fd7ec7c9d9db2be2c005cec0b
Jeba-anika <anikanur98@gmail.com>
2022-10-25 12:10:07 +0600

    course-category json file created

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ app.get('/', (req, res)=>{
     res.send('Tech Learners API running')
 })
 
-app.get('/course-category', (req, res)=>{
-    res.send(categories)
-})
-
-
-
-app.get('/all-courses', (req, res)=>{
-    res.send(allCourses)
-})
-
-app.get('/all-courses/:id', (req, res)=>{
-    const id = req.params.id;
-    const selectedCourse = allCourses.find(course => course._id === id)
-    res.send(selectedCourse)
-})
 
 app.listen(port, ()=>{
     console.log('Tech Learners Server is running at port:', port)
