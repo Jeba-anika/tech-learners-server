e1bf4236fefd0eeaceb0608e6a1249502edef7f0
Jeba-anika <anikanur98@gmail.com>
2022-10-25 12:15:45 +0600

    server side routes created

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const allCourses = require('./data/all-course-details.json')
 app.get('/', (req, res)=>{
     res.send('Tech Learners API running')
 })
+app.get('/course-category', (req, res)=>{
+    res.send(categories)
+})
+app.get('/all-courses', (req, res)=>{
+    res.send(allCourses)
+})
+
+app.get('/all-courses/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = allCourses.find(course => course._id === id)
+    res.send(selectedCourse)
+})
 
 
 app.listen(port, ()=>{
